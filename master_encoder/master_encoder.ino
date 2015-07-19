2560c53837cac411181a270497089f5f0060bca3
mimouncadosch <mimouncadosch@gmail.com>
2015-07-19 11:42:50 -0400

    direction and speed encoders combined

--- master_encoder/master_encoder.ino
+++ master_encoder/master_encoder.ino
@@ -1,6 +1,4 @@
-#include <Thread.h>
-#include <ThreadController.h>
-#include <TimerOne.h>
+
 
 // Above threshold: HIGH, Below threshold: LOW
 int threshold = 2.5;
@@ -11,81 +9,87 @@ int readSensor(int pin) {
   return (voltage >= threshold) ? HIGH : LOW;
 }
 
-class DirectionEncoderThread: public Thread {
-   public: 
-      // Direction of wheel rotation: +1 forward; -1 backward
-      int dir;  
-      // Pins from which analog values will be read
-      int pinA;
-      int pinB;
-      // Sensor reading values
-      int n;              // New sensor reading
-      int last_n;         // Previous sensor reading
-      
-      void run() {
-          n = readSensor(pinB);  
-          if ( (last_n == LOW) && (n == HIGH) ) {
-              if ( readSensor(pinA) == LOW )  {
-                  dir = -1;
+class SpeedEncoder  {
+    // Class variables
+    int pin;
+    int revs;
+    int n;
+    int last_n;
+    int updateInterval;      // interval between updates
+    unsigned long lastUpdate; // last update of position
+    
+    // Constructor
+    public:
+        SpeedEncoder(int myPin, int interval) {
+            pin = myPin;
+            last_n = LOW;
+            updateInterval = interval;
+        }
+        
+        int update() {
+          if((micros() - lastUpdate) > updateInterval) { // time to update
+              lastUpdate = micros();
+              
+              // Update sensor value
+              n = readSensor(pin);    
+              if (n != last_n) {
+                  revs += 1;
+//                  Serial.println(revs);
+                  last_n = n;
               }
-              else {
-                  dir = 1;
-              }
+          }
+        }
+};
+
+class DirectionEncoder {
+    // Class variables 
+    int pinA;
+    int pinB;
+    int dir;
+    int n;
+    int last_n;
+    int updateInterval;
+    unsigned long lastUpdate;
+    
+    // Constructor
+    public:
+        DirectionEncoder(int myPinA, int myPinB, int interval) {
+            pinA = myPinA;
+            pinB = myPinB;
+            last_n = LOW;
+            updateInterval = interval;
+        }
+        
+        int update() {
+          if((micros() - lastUpdate) > updateInterval) { // time to update
+              lastUpdate = micros();
+              
+              // Update sensor value
+              n = readSensor(pinB);  
+              if ( (last_n == LOW) && (n == HIGH) ) {
+                if ( readSensor(pinA) == LOW )  {
+                    dir = -1;
+                }
+                else {
+                    dir = 1;
+                }
 //              Serial.println(dir);
           }
-          last_n = n;
-          runned();
-      }       
+          last_n = n;  
+          }  
+        }
 };
 
-class SpeedEncoder {
-  public:   
-    int pin;
-      int revs;
-      int n;
-      int last_n = LOW;
-      
-      void update() {
-        n = readSensor(pin);    
-        if (n != last_n) {
-          revs += 1;
-          Serial.println(revs);
-          last_n = n;
-        }  
-      }
-};
-DirectionEncoderThread directionEncoder = DirectionEncoderThread();
-SpeedEncoder speedEncoder = SpeedEncoder();
-
-ThreadController controller = ThreadController();
-
-void timerCallback() {
-    controller.run(); 
-}
-
+SpeedEncoder se = SpeedEncoder(A3, 100);
+DirectionEncoder de = DirectionEncoder(A0, A1, 100);
+int revs;
+int dir;
 void setup() {
     Serial.begin(9600);
-    
-    // Set up speed encoder class variables
-    directionEncoder.dir = 0;
-    directionEncoder.pinA = A0;
-    directionEncoder.pinB = A1;
-    directionEncoder.last_n = LOW;
-    directionEncoder.setInterval(1);
-    
-    speedEncoder.pin = A3;
-    speedEncoder.revs = 0;
-    speedEncoder.last_n = LOW;
-    
-    // Add the Threads to our ThreadController
-    controller.add(&directionEncoder);
-    
-    Timer1.initialize(20000);
-    Timer1.attachInterrupt(timerCallback);
-    Timer1.start();
-    
 }
 
-void loop(){
-  speedEncoder.update();
+void loop() {
+    revs = se.update();
+//    dir = de.update();
+    Serial.println(revs);
 }
