55333455290ca44a4df0460503d37d99053bf394
mimouncadosch <mimouncadosch@gmail.com>
2015-07-22 13:14:00 -0400

    no delaying one wheel works

--- master_encoder/master_encoder.ino
+++ master_encoder/master_encoder.ino
@@ -10,36 +10,27 @@ int readSensor(int pin) {
 class SpeedEncoder  {
     // Class variables
     int pin;
-    int n;
     int revs = 0;
+    int n;
     int last_n;
-    int updateInterval;      // interval between updates
-    unsigned long lastUpdate; // last update of position
     
     // Constructor
     public:
-        SpeedEncoder(int myPin, int interval) {
+        SpeedEncoder(int myPin) {
             pin = myPin;
             last_n = LOW;
-            updateInterval = interval;
         }
         
         int update() {
-          if((micros() - lastUpdate) > updateInterval) { // time to update
-              lastUpdate = micros();
               // Update sensor value
               n = readSensor(pin);    
+              
               if (n != last_n) {
-                  last_n = n;
                   revs += 1;
-                  return 1;
+                  Serial.println(revs);
+                  last_n = n;
               }
-              else { 
-                  return 0; 
-              }
-          }
-//        return revs;
-        }
+         }     
 };
 
 class DirectionEncoder {
@@ -49,66 +40,47 @@ class DirectionEncoder {
     int dir;  // Forward direction by default
     int n;
     int last_n;
-    int updateInterval;
-    unsigned long lastUpdate;
     
     // Constructor
     public:
-        DirectionEncoder(int myPinA, int myPinB, int interval) {
+        DirectionEncoder(int myPinA, int myPinB) {
             pinA = myPinA;
             pinB = myPinB;
             last_n = LOW;
             dir = 1;
-            updateInterval = interval;
         }
         
         int update() {
-          if((micros() - lastUpdate) > updateInterval) { // time to update
-              lastUpdate = micros();
-              
               // Update sensor value
               n = readSensor(pinB);  
               if ( (last_n == LOW) && (n == HIGH) ) {
                 if ( readSensor(pinA) == LOW )  {
-                    dir = -1;
+                    dir = 1;
+                    Serial.println(dir);
                 }
                 else {
-                    dir = 1;
+                    dir = -1;
+                    Serial.println(dir);
                 }
           }
-          last_n = n;  
-          }  
-        return dir;
+          last_n = n;
+          return dir;
         }
 };
 
 // Right wheel
-SpeedEncoder se_right;
-DirectionEncoder de_right;
+SpeedEncoder se_right = SpeedEncoder(A0);
+DirectionEncoder de_right = DirectionEncoder(A4,A5);
+
 
 void setup() {
     Serial.begin(9600);
     Serial.println("Setting up");
-    se_right = SpeedEncoder(A0, 15);
-    de_right = DirectionEncoder(A5, A4, 15);
     delay(1000);
     Serial.println("Ready");
 }
 
-int totRevs = 0;
-int last_dir = 1;
-int dir = 1;
-int compensationRevs = 20;
-
-int compensated = false;
-void loop() {  
-  
-//    if (dir != last_dir && compensated == false) {
-//      Serial.println("dir != last_dir");
-//      totRevs -= compensationRevs;
-//      compensated = true;
-//    } 
-//    dir = de_right.update();
-//    totRevs += se_right.update() * de_right.update();
-//    Serial.println(totRevs);
+void loop() {
+    se_right.update();
+//    de_right.update();
 }
