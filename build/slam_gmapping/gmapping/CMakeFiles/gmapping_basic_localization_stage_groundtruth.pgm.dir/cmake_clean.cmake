7ac051f299206106b6ecb80b89af0b1e811e8c0b
mimouncadosch <mimouncadosch@gmail.com>
2015-07-30 05:02:55 -0400

    added rest of ros packages

---
+++ build/slam_gmapping/gmapping/CMakeFiles/gmapping_basic_localization_stage_groundtruth.pgm.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gmapping_basic_localization_stage_groundtruth.pgm"
+  "/home/rbtying/finalize_wheelchair/devel/share/gmapping/test/basic_localization_stage_groundtruth.pgm"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/gmapping_basic_localization_stage_groundtruth.pgm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
